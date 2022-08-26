82afa32d495a5c5d381bc1d80da13a9d34fb0bd9
Juanizo <sebastiangonzalezciro@gmail.com>
2022-08-26 07:13:14 -0500

    Agregando archivos

---
+++ EducacionVirtual.App/EducacionVirtual.App.Dominio/Entidades/Persona.cs
@@ -0,0 +1,16 @@
+
+namespace EducacionVirtual.App.Dominio
+{
+    public class Persona()
+    {
+       public int Id{get;set;} 
+       public string Nombre{get;set;}
+       public string Apellido{get;set;}
+       public string  Email {get;set;}
+       public int Edad {get;set;}
+       public string Telefono{get;set;}
+       public string Direccion{get;set;}
+       public string Correo{get;set;}
+       
+    }
+}
---
+++ EducacionVirtual.App/EducacionVirtual.App.Persistencia/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace EducacionVirtual.App.Persistencia
+{
+    public class Class1
+    {
+    }
+}
