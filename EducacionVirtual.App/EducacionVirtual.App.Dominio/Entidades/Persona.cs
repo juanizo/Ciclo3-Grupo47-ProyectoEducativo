6683d3add7bcf98a8880209bfc2b8fcd928afc50
Juanizo <sebastiangonzalezciro@gmail.com>
2022-08-28 18:53:08 -0500

    Actualizar

---
+++ EducacionVirtual.App/EducacionVirtual.App.Dominio/Entidades/Persona.cs
@@ -0,0 +1,16 @@
+using System;
+namespace EducacionVirtual.App.Dominio
+{
+    public class Persona{
+        public int Id{get;set;}
+        public string Name{get;set;}
+        public string Apellidos{get;set;}
+        public string Email{get;set;}
+        public int Edad{get;set;}
+        public string Telefono{get;set;}
+        public string Direccion{get;set;}
+        public string Usuario{get;set;}
+        public string Contrasena{get;set;}
+        public string Codigo{get;set;}
+    }
+}
