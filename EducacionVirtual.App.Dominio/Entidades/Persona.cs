d4a1226613efaedac11f62d62493fb513d27cc92
Juanizo <sebastiangonzalezciro@gmail.com>
2022-09-11 23:53:41 -0500

    Detalles Diseño, Registro

--- EducacionVirtual.App.Dominio/Entidades/Persona.cs
+++ EducacionVirtual.App.Dominio/Entidades/Persona.cs
@@ -1,16 +1,26 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 namespace EducacionVirtual.App.Dominio
 {
     public class Persona{
         public int Id{get;set;}
+        [Required]
         public string Nombre{get;set;}
+        [Required]
         public string Apellidos{get;set;}
+        [Required]
         public string Email{get;set;}
+        [Required]
         public int Edad{get;set;}
+        [Required]
         public string Telefono{get;set;}
+        [Required]
         public string Direccion{get;set;}
+        [Required]
         public string Usuario{get;set;}
+        [Required]
         public string Contrasena{get;set;}
+        [Required]
         public string Codigo{get;set;}
     }
 }
--- EducacionVirtual.App.Dominio/Entidades/Profesor.cs
+++ EducacionVirtual.App.Dominio/Entidades/Profesor.cs
@@ -1,9 +1,10 @@
 using System;
-
+using System.ComponentModel.DataAnnotations;
 namespace EducacionVirtual.App.Dominio{
 
     public class Profesor : Persona
     {
+        [Required]
         public string Especializacion{get;set;}
         public Grado Grado{get;set;}
     }
